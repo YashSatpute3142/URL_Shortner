bed911dc55380cdaaf1e84fd53696266c883e4c7
Yash Satpute <yashsatpute2006@gmail.com>
2026-07-21 18:09:16 +0530

    fixing port for deploy

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ import path from "path";
 
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 const DATA_FILE = path.join("data", "links.json");
 
 const serveFile = async(res,filePath, contentType) => {
